39591783e7971e0cb3893c2c3bdf5fac2be4662c
Zachary C <zcutting22@gmail.com>
2022-10-07 15:16:37 -0400

    Defined a grid using css and javascript

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-console.log("Testing")
+const gridContainer = document.querySelector('.grid-container');
+
+for(let i = 0; i < 256; i++){
+    const gridItem = document.createElement('div');
+    gridItem.className = 'grid-item';
+    gridContainer.appendChild(gridItem);
+}
