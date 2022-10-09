1a72ae1f18500fbe6f320753d1ba0070946e1500
Zachary C <zcutting22@gmail.com>
2022-10-09 11:38:13 -0400

    Created a function that would fill grid container with grid items

--- script/script.js
+++ script/script.js
@@ -1,4 +1,21 @@
 //Get the drawing pad and grid container
 const pad = document.querySelector('.pad');
 const gridContainer = document.querySelector('.grid-container');
-console.log(gridContainer);
+
+//Fill grid container with grid items
+let width = 64;
+let height = 64;
+let wXh = width * height;
+
+function fillGrid() {
+    for (let i = 1; i <= wXh; i++){
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.backgroundColor = 'white';
+        })
+        gridContainer.appendChild(gridItem);
+    }
+}
+
+fillGrid();
